4c9a90e0547a31486aeee7161c47154873e82312
AlvaroTorresVazquez <alvaro.torres.138@gmail.com>
2025-08-30 10:27:59 -0600

    Add files via upload
    Practica Blog

---
+++ app_blog/views.py
@@ -0,0 +1,12 @@
+#from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Publicacion  # Importa el modelo
+
+def index(request):
+    # Obtener todas las publicaciones de la base de datos
+    publicaciones = Publicacion.objects.all()
+    # Renderiza la plantilla y pasa los datos
+    return render(request, 'app_blog/index.html', {'publicaciones': publicaciones})
+
